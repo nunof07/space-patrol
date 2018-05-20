c1aa03bedf0fae09b6bc29bd62c5a9626fffb889
Nuno Freitas <nunofreitas@gmail.com>
2018-05-20 11:06:24 +0100

    test(jsdom): mock Image

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,10 +1,10 @@
 const { JSDOM } = require('jsdom');
 const copyProps = require('./copyProps');
 const mockCanvas = require('./mockCanvas');
+//const mockImage = require('./mockImage');
 
-const jsdom = new JSDOM(
-    '<!doctype html><html><body><div id="game"></div></body></html>'
-);
+const html = '<!doctype html><html><body><div id="game"></div></body></html>';
+const jsdom = new JSDOM(html, { resources: 'usable' });
 const { window } = jsdom;
 
 global.window = window;
@@ -12,5 +12,6 @@ global.document = window.document;
 global.navigator = {
     userAgent: 'node.js',
 };
-copyProps(window, global);
+//mockImage(window, window.Image, './src/assets');
 mockCanvas(window);
+copyProps(window, global);
