b11e1f68ea9016c42ef97efc32d255b84f9264eb
Nuno Freitas <nunofreitas@gmail.com>
2018-05-28 22:58:39 +0100

    refactor(player): remove code duplication

--- src/sprites/addPlayer.ts
+++ src/sprites/addPlayer.ts
@@ -1,74 +1,86 @@
 import * as Phaser from 'phaser';
 
+type SpriteConfig = {
+    key: string;
+    offset: { x: number; y: number };
+    depth: number;
+};
+
 function addCockpit(
     scene: Phaser.Scene,
-    centerX: number,
-    centerY: number
+    center: { x: number; y: number }
 ): Phaser.GameObjects.Sprite {
-    return scene.make.sprite({
-        x: centerX,
-        y: centerY,
-        depth: 1000,
-        key: 'player-cockpit',
-        flipY: true,
-    });
+    return addPlayerSprite(
+        scene,
+        center,
+        {
+            key: 'player-cockpit',
+            offset: { x: 0, y: 0 },
+            depth: 1000,
+        },
+        false
+    );
 }
 
 function addEngine(
     scene: Phaser.Scene,
-    centerX: number,
-    centerY: number
+    center: { x: number; y: number }
 ): Phaser.GameObjects.Sprite {
-    return scene.make.sprite({
-        x: centerX,
-        y: centerY + 37,
-        depth: 990,
-        key: 'player-engine',
-        flipY: true,
-    });
+    return addPlayerSprite(
+        scene,
+        center,
+        {
+            key: 'player-engine',
+            offset: { x: 0, y: 37 },
+            depth: 990,
+        },
+        false
+    );
 }
 
-function addBeam(
+function addPlayerSprite(
     scene: Phaser.Scene,
-    centerX: number,
-    centerY: number,
+    center: { x: number; y: number },
+    config: SpriteConfig,
     isLeft: boolean
 ): Phaser.GameObjects.Sprite {
     return scene.make.sprite({
-        x: centerX + (isLeft ? -1 : 1) * 27,
-        y: centerY + 9,
-        depth: 990,
-        key: 'player-beam',
+        x: center.x + (isLeft ? -1 : 1) * config.offset.x,
+        y: center.y + config.offset.y,
+        depth: config.depth,
+        key: config.key,
         flipX: isLeft,
         flipY: true,
     });
 }
 
-function addWing(
-    scene: Phaser.Scene,
-    centerX: number,
-    centerY: number,
-    isLeft: boolean
-): Phaser.GameObjects.Sprite {
-    return scene.make.sprite({
-        x: centerX + (isLeft ? -1 : 1) * 51,
-        y: centerY - 7,
-        depth: 1000,
+function wing(): SpriteConfig {
+    return {
         key: 'player-wing',
-        flipX: isLeft,
-        flipY: true,
-    });
+        offset: { x: 51, y: -7 },
+        depth: 1000,
+    };
+}
+
+function beam(): SpriteConfig {
+    return {
+        key: 'player-beam',
+        offset: { x: 27, y: 9 },
+        depth: 990,
+    };
 }
 
 export function addPlayer(scene: Phaser.Scene): Phaser.GameObjects.Group {
-    const centerX = scene.cameras.main.width * 0.5;
-    const centerY = scene.cameras.main.height * 0.5;
-    const cockpit = addCockpit(scene, centerX, centerY);
-    const engine = addEngine(scene, centerX, centerY);
-    const beamLeft = addBeam(scene, centerX, centerY, true);
-    const beamRight = addBeam(scene, centerX, centerY, false);
-    const wingLeft = addWing(scene, centerX, centerY, true);
-    const wingRight = addWing(scene, centerX, centerY, false);
+    const center = {
+        x: scene.cameras.main.width * 0.5,
+        y: scene.cameras.main.height * 0.5,
+    };
+    const cockpit = addCockpit(scene, center);
+    const engine = addEngine(scene, center);
+    const beamLeft = addPlayerSprite(scene, center, beam(), true);
+    const beamRight = addPlayerSprite(scene, center, beam(), false);
+    const wingLeft = addPlayerSprite(scene, center, wing(), true);
+    const wingRight = addPlayerSprite(scene, center, wing(), false);
     const group = scene.add.group({});
     group.addMultiple([
         cockpit,
