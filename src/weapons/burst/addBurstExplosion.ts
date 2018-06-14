b164ad44ddc95e830612612001f2bb14b64cf547
Nuno Freitas <nunofreitas@gmail.com>
2018-06-14 18:35:38 +0100

    refactor(anim): addAnimation

--- src/weapons/burst/addBurstExplosion.ts
+++ src/weapons/burst/addBurstExplosion.ts
@@ -1,15 +1,6 @@
+import { addAnimation } from '@src/animation';
 import * as Phaser from 'phaser';
 
 export function addBurstExplosion(scene: Phaser.Scene): void {
-    scene.anims.create({
-        key: 'burstExplosion',
-        frames: scene.anims.generateFrameNames('sprites', {
-            prefix: 'player/burst_explosion_',
-            suffix: '.png',
-            end: 9,
-            zeroPad: 4,
-        }),
-        frameRate: 24,
-        hideOnComplete: true,
-    });
+    addAnimation(scene, 'burstExplosion', 'player/burst_explosion_', 9);
 }
