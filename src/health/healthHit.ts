d40c344f2f75092bbc93560bc5b8560a2b36bd05
Nuno Freitas <nunofreitas@gmail.com>
2018-06-17 16:02:12 +0100

    fix(health): damage no longer can take both shield and health

--- src/health/healthHit.ts
+++ src/health/healthHit.ts
@@ -1,18 +1,18 @@
 import { Hitpoints } from '@src/health/Hitpoints';
 import { Vitality } from '@src/health/Vitality';
 
-export function healthHit(health: Vitality, amount: number): Vitality {
-    // amount to be taken from shield
-    const shieldAmount =
-        amount > health.shield.current ? health.shield.current : amount;
-    const newShield = health.shield.current - shieldAmount;
+export function healthHit(vitality: Vitality, amount: number): Vitality {
+    let newHealth: number = vitality.health.current;
+    let newShield: number = vitality.shield.current;
 
-    // amount to be taken from health
-    const healthAmount = amount - shieldAmount;
-    const newHealth = Math.max(0, health.health.current - healthAmount);
+    if (vitality.shield.current > 0) {
+        newShield = Math.max(0, vitality.shield.current - amount);
+    } else {
+        newHealth = Math.max(0, vitality.health.current - amount);
+    }
 
     return new Vitality(
-        new Hitpoints(health.health.max, newHealth),
-        new Hitpoints(health.shield.max, newShield)
+        new Hitpoints(vitality.health.max, newHealth),
+        new Hitpoints(vitality.shield.max, newShield)
     );
 }
